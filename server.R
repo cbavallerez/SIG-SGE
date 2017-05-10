e92bee2027da35bb5376f4612411db1f3a7225f5
cbavallerez <cbavallerez@gmail.com>
2017-05-10 16:08:41 -0300

    Se pueden seleccionar los establecimientos a mostrar por comuna o region

--- server.R
+++ server.R
@@ -16,27 +16,58 @@ ubicacion_establecimiento <- function(txtregion) {
   ubicacion <- dbGetQuery(conn,my_query)
 }
 
-comunas <- function(txtregion) {
+
+ubicacion_establecimiento_comuna <- function(txtcomuna) {
+  
   #la consulta extra el RBD, Langitud y latitud de todos los establecimientos de la region 
-  my_query <- 'SELECT NOM_COM, COD_COMUNA FROM COMUNAS WHERE COD_REG = TXTREGION'
-  my_query <- sub("TXTREGION",txtregion,my_query)
+  my_query <- 'SELECT RBD, LATITUD, LONGITUD FROM ESTABLECIMIENTOS WHERE COD_COM_RBD = TXTCOMUNA'
+  my_query <- sub("TXTCOMUNA",txtcomuna,my_query)
   ubicacion <- dbGetQuery(conn,my_query)
 }
+
+
+
 
 
 shinyServer(function(input, output) {
   conn <- dbConnect(MySQL(), user="root", host="localhost", password="03121991-0oK", dbname="mydb", port=3306)
   
+  comunas <- function(txtregion) {
+    #la consulta extra el RBD, Langitud y latitud de todos los establecimientos de la comuna 
+    my_query <- 'SELECT NOM_COM, COD_COMUNA FROM COMUNAS WHERE COD_REG = TXTREGION'
+    my_query <- sub("TXTREGION",txtregion,my_query)
+    ubicacion <- dbGetQuery(conn,my_query)
+  }
   
+  #En la siguiente salida se crea un select para las comunas
+  output$seleccionar_comuna = renderUI({
+    RBD_establecimiento_seleccionado <- as.numeric(input$establecimiento_seleccionado) 
+    choicesComunas <- setNames(as.character(comunas(input$txtregion)$COD_COMUNA), comunas(input$txtregion)$NOM_COM)
+    if(is.null(RBD_establecimiento_seleccionado)){
+      selectInput("txtcomuna", label = h4("Seleccionar Comuna"), choices = choicesComunas
+      )
+    }else{
+      #selectInput("txtcomuna", label = h4("Seleccionar Comuna"), choices = choicesComunas, selected = as.numeric(establecimiento_seleccionado(RBD_establecimiento_seleccionado)$COD_COM_RBD) 
+      #)
+    }
+    
+    
+    
+  })
   #En la siguiente salida se envia a la UI.R un input del tipo numerico que servira pa seleecionar un establecimiento 
   output$inputRBD = renderUI({
     numericInput("establecimiento_seleccionado", "RBD:",  5666)
   })
-  
+  output$salida_ver_shapefile<-renderPrint({
+    if (input$estab_for_region == FALSE) {
+      input$txtcomuna
+    } else {
+      input$txtcomuna
+    }
+  })
   
   output$salida_estab_for_region <- renderText({ input$estab_for_region })
-  output$salida_ver_shapefile <- renderText({ input$ver_shapefile })
-  
+
   
   output$establecimiento_datos = renderUI({
     
@@ -66,6 +97,7 @@ shinyServer(function(input, output) {
     
     
   })
+
   
   #Envia informacion sobre los establecimientos a una tabla 
   output$DatosEstablecimiento = renderDataTable({
@@ -76,6 +108,7 @@ shinyServer(function(input, output) {
   
   #Toma informacion del establecimiento seleccionado en el input RBD
   establecimiento_seleccionado <- function(RBD_establecimiento_seleccionado) {
+    
     RBD_establecimiento_seleccionado <- as.numeric(input$establecimiento_seleccionado) 
     conn <- dbConnect(MySQL(), user="root", host="localhost", password="03121991-0oK", dbname="mydb", port=3306)
     
@@ -115,11 +148,17 @@ shinyServer(function(input, output) {
   
   
   ######################################    MAPA ############################################
-  
+  output$oid2<-renderPrint({
+    if (x()>50) {
+      "You entered a number greater than 10"
+    } else {
+      "You entered a number less than or equal to 10"
+    }
+  })
   
   
   #  Se declara la salida mimapa almacenando un renderleaflet
-  output$mimapa <- renderLeaflet({
+  output$mimapa <- renderLeaflet({ 
     
     
     #Se inicia la conexion con la base de datos
@@ -144,16 +183,37 @@ shinyServer(function(input, output) {
     leaflet() %>% addTiles()%>%setView(lng = posicionregion$LONGITUD, lat = posicionregion$LATITUD, zoom = 8)
     #cons<-dbListConnections(MySQL()) for(con in cons) dbDisconnect(con)
   })
-  observe({
-    
-    txtregion <-input$txtregion
-    
-    
-    leafletProxy("mimapa", data = ubicacion_establecimiento(txtregion)) %>%
-      clearShapes() %>%
-      addCircleMarkers(lat =  ~LATITUD, lng =  ~LONGITUD, weight = 6, radius = 3,color="#ffa500", stroke = TRUE, fillOpacity = 0.8, layerId = ~RBD
-      )
-  })
+
+    observe({
+      
+      if (input$estab_for_region == TRUE) {
+        txtregion <-input$txtregion
+        
+        
+        leafletProxy("mimapa", data = ubicacion_establecimiento(txtregion)) %>%
+          clearMarkers() %>%
+          addCircleMarkers(lat =  ~LATITUD, lng =  ~LONGITUD, weight = 6, radius = 3,color="#ffa500", stroke = TRUE, fillOpacity = 0.8, layerId = ~RBD
+          )
+      } else {
+        
+        
+        if(is.null(input$txtcomuna)){
+          txtcomuna <- "9119"
+        } else{
+          txtcomuna <-input$txtcomuna
+          
+       
+        }
+        
+        leafletProxy("mimapa", data = ubicacion_establecimiento_comuna(txtcomuna)) %>%
+          clearMarkers() %>%
+          addCircleMarkers(lat =  ~LATITUD, lng =  ~LONGITUD, weight = 6, radius = 3,color="#ffa500", stroke = TRUE, fillOpacity = 0.8, layerId = ~RBD
+          )
+      }
+      
+
+    })
+
   
   observeEvent(input$mimapa_marker_click, { # update the map markers and view on map clicks
     p <- input$mimapa_marker_click
